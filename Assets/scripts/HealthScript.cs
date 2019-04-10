811b1386f2f930cea8116c4d58e4b685b4ab68e4
james kennedy <jameskennedy@jamess-MacBook-Air.local>
2019-04-10 19:17:20 +0100

    got lives system working and added EndMenu

---
+++ Assets/scripts/HealthScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HealthScript : MonoBehaviour
+{
+
+    private int health = 3;
+    public GameObject live1, live2, live3;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "EnemyBullet" )
+        {
+            
+            Object.Destroy(col.gameObject);
+            //Object.Destroy(this.gameObject);
+            health -= 1;
+
+
+
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        live1.gameObject.SetActive(true);
+        live2.gameObject.SetActive(true);
+        live3.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthController();
+    }
+
+
+    void healthController(){
+
+        if(health > 3){
+            health = 3;
+        }
+
+        switch(health){
+            case 3:
+                live1.gameObject.SetActive(true);
+                live2.gameObject.SetActive(true);
+                live3.gameObject.SetActive(true);
+                break;
+            case 2:
+                live1.gameObject.SetActive(true);
+                live2.gameObject.SetActive(true);
+                live3.gameObject.SetActive(false);
+                break;
+            case 1:
+                live1.gameObject.SetActive(true);
+                live2.gameObject.SetActive(false);
+                live3.gameObject.SetActive(false);
+                break;
+            case 0:
+                live1.gameObject.SetActive(false);
+                live2.gameObject.SetActive(false);
+                live3.gameObject.SetActive(false);
+                EndGame();
+                break;
+            }
+    }
+
+    void EndGame(){
+        SceneManager.LoadSceneAsync("EndMenu");
+    }
+}
